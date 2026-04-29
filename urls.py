513f5357f8239ef2d95023b4cc27889dd3f8c3a9
sujankakadiya <kakadiyasujan68@gmail.com>
2026-04-29 10:36:29 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -5,8 +5,6 @@ from wscubtech import views
 
 
 urlpatterns = [
-    path('',views.Home),
-    path('admin/', admin.site.urls),
-    path('aboutUs/',views.aboutUs,name='aboutUs'),
+   
     path('calculator/',views.calculator,name='calculator'),
 ]
